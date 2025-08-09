1d76a6925f20e87f8b34c6368feb516aa5f7fa23
jesicag2 <jesicagarcia109@gmail.com>
2025-08-09 13:36:03 -0400

    questions added

---
+++ app.js
@@ -0,0 +1,37 @@
+const quizData = [
+    {
+        question: "Which examines how individuals, institutions, or society make decisions under conditions of scarcity?",
+        options: ["Management", "Economics", "Finance", "Accounting"],
+        answer: 1
+    },
+    {
+        question: "When unlimited wants exceed the limited resources available to fulfill those wants.",
+        options: ["Efficiency", "Uniqueness", "Scarcity", "Capital"],
+        answer: 2
+    },
+    {
+        question: "What is a payment for labor?",
+        options: ["Wages", "Taxes", "Rent", "Interest"],
+        answer: 0
+    },
+    {
+        question: "Goods are:",
+        options: ["Financial", "Human", "Rare", "Tangible"],
+        answer: 3
+    },
+    {
+        question: "What entails rational decision making?",
+        options: ["Process of ignoring potential outcomes.", "Process of making a pro/con list.", "Method of making random decisions.", "Technique of relying on intuition only."],
+        answer: 1
+    },
+    {
+        question: "What is oppurtunity cost?",
+        options: ["The money spent on a purchase.", "The price of the product.", "The amount of money earned.", "What you give up when you make a choice."],
+        answer: 3
+    },
+    {
+        question: "Services are:",
+        options: ["Movable", "Financial", "Intangible", "Tangible"],
+        answer: 2
+    }
+];
